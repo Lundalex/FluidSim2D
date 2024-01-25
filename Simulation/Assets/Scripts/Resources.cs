5c6ac4d27af0c625a74b289a11b304d9373cb5c5
Noah Marklund <xtreamnerdspvp@gmail.com>
2024-01-25 23:38:45 +0100

    Code refactor

---
+++ Simulation/Assets/Scripts/Resources.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+using Unity.Mathematics;
+using System;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+namespace Resources
+{
+    public struct BufferDataStruct
+    {
+        public string bufferName;
+        public ComputeBuffer buffer;
+    }
+    public struct SpringStruct
+    {
+        public int linkedIndex;
+        public float restLength;
+        // public float yieldLen;
+        // public float plasticity;
+        // public float stiffness;
+    };
+    public struct StickynessRequestStruct
+    {
+        public int pIndex;
+        public int StickyLineIndex;
+        public float2 StickyLineDst;
+        public float absDstToLineSqr;
+        public float RBStickyness;
+        public float RBStickynessRange;
+    };
+    public struct PDataStruct
+    {
+        public float2 PredPosition;
+        public float2 Position;
+        public float2 Velocity;
+        public float2 LastVelocity;
+        public float Density;
+        public float NearDensity;
+        public int PType;
+    }
+    public struct PTypeStruct
+    {
+        public float TargetDensity;
+        public int MaxInfluenceRadius;
+        public float Pressure;
+        public float NearPressure;
+        public float Damping;
+        public float Viscocity;
+        public float Elasticity;
+        public float Plasticity;
+        public float Stickyness;
+        public float Gravity;
+        public float colorG;
+    };
+    public struct RBDataStruct
+    {
+        public float2 Position;
+        public float2 Velocity;
+        // radians / second
+        public float AngularImpulse;
+
+        public float Stickyness;
+        public float StickynessRange;
+        public float StickynessRangeSqr;
+        public float2 NextPos;
+        public float2 NextVel;
+        public float NextAngImpulse;
+        public float Mass;
+        public int2 LineIndices;
+        public float MaxDstSqr;
+        public int WallCollision;
+        public int Stationary; // 1 -> Stationary, 0 -> Non-stationary
+    };
+    public struct RBVectorStruct
+    {
+        public float2 Position;
+        public float2 LocalPosition;
+        public float3 ParentImpulse;
+        public int ParentRBIndex;
+        public int WallCollision;
+    };
+
+    public class Utils
+    {
+        public static Vector2 GetMouseWorldPos(int Width, int Height)
+        {
+            Vector3 MousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x , Input.mousePosition.y , -Camera.main.transform.position.z));
+            Vector2 MouseWorldPos = new(((MousePos.x - Width/2) * 0.55f + Width) / 2, ((MousePos.y - Height/2) * 0.55f + Height) / 2);
+
+            return MouseWorldPos;
+        }
+
+        public static float2 ParticleSpawnPosition(int pIndex, int maxIndex, int Width, int Height, int SpawnDims)
+        {
+            float x = (Width - SpawnDims) / 2 + Mathf.Floor(pIndex % Mathf.Sqrt(maxIndex)) * (SpawnDims / Mathf.Sqrt(maxIndex));
+            float y = (Height - SpawnDims) / 2 + Mathf.Floor(pIndex / Mathf.Sqrt(maxIndex)) * (SpawnDims / Mathf.Sqrt(maxIndex));
+            if (SpawnDims > Width || SpawnDims > Height)
+            {
+                throw new ArgumentException("Particle spawn dimensions larger than either border_width or border_height");
+            }
+            return new float2(x, y);
+        }
+
+        public static bool2 GetMousePressed()
+        {
+            bool LMousePressed = Input.GetMouseButton(0);
+            bool RMousePressed = Input.GetMouseButton(1);
+
+            bool2 MousePressed = new bool2(LMousePressed, RMousePressed);
+
+            return MousePressed;
+        }
+    }
+}
---
+++ Simulation/Assets/Scripts/ShaderHelper.cs
@@ -0,0 +1,203 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderHelper : MonoBehaviour
+{
+    public Main m;
+    public void SetPSimShaderBuffers(ComputeShader pSimShader)
+    {
+        if (m.ParticlesNum != 0) {
+            // Kernel PreCalculations
+            pSimShader.SetBuffer(0, "PData", m.PDataBuffer);
+            pSimShader.SetBuffer(0, "PTypes", m.PTypesBuffer);
+        
+            // Kernel PreCalculations
+            pSimShader.SetBuffer(1, "SpatialLookup", m.SpatialLookupBuffer);
+            pSimShader.SetBuffer(1, "StartIndices", m.StartIndicesBuffer);
+
+            pSimShader.SetBuffer(1, "PData", m.PDataBuffer);
+            pSimShader.SetBuffer(1, "PTypes", m.PTypesBuffer);
+
+            // Kernel ParticleForces
+            pSimShader.SetBuffer(2, "SpatialLookup", m.SpatialLookupBuffer);
+            pSimShader.SetBuffer(2, "StartIndices", m.StartIndicesBuffer);
+
+            pSimShader.SetBuffer(2, "SpringPairs", m.SpringPairsBuffer);
+            pSimShader.SetBuffer(2, "PData", m.PDataBuffer);
+            pSimShader.SetBuffer(2, "PTypes", m.PTypesBuffer);
+            pSimShader.SetBuffer(2, "RBData", m.RBDataBuffer);
+            pSimShader.SetBuffer(2, "RBVector", m.RBVectorBuffer); 
+
+            pSimShader.SetBuffer(3, "PData", m.PDataBuffer);
+            pSimShader.SetBuffer(3, "PTypes", m.PTypesBuffer);
+
+            pSimShader.SetBuffer(4, "PData", m.PDataBuffer);
+            pSimShader.SetBuffer(4, "PTypes", m.PTypesBuffer);
+            pSimShader.SetBuffer(4, "SortedStickyRequests", m.SortedStickyRequestsBuffer); 
+        }
+    }
+
+    public void SetRbSimShaderBuffers(ComputeShader rbSimShader)
+    {
+        if (m.RBodiesNum != 0)
+        {
+            rbSimShader.SetBuffer(0, "RBVector", m.RBVectorBuffer);
+            rbSimShader.SetBuffer(0, "RBData", m.RBDataBuffer);
+
+            rbSimShader.SetBuffer(1, "RBVector", m.RBVectorBuffer);
+            rbSimShader.SetBuffer(1, "RBData", m.RBDataBuffer);
+            rbSimShader.SetBuffer(1, "TraversedChunksAPPEND", m.TraversedChunks_AC_Buffer);
+
+            // Maximum reached! (8)
+            rbSimShader.SetBuffer(2, "PData", m.PDataBuffer);
+            rbSimShader.SetBuffer(2, "PTypes", m.PTypesBuffer);
+            rbSimShader.SetBuffer(2, "RBData", m.RBDataBuffer);
+            rbSimShader.SetBuffer(2, "RBVector", m.RBVectorBuffer);
+            rbSimShader.SetBuffer(2, "SpatialLookup", m.SpatialLookupBuffer);
+            rbSimShader.SetBuffer(2, "StartIndices", m.StartIndicesBuffer);
+            rbSimShader.SetBuffer(2, "TraversedChunksCONSUME", m.TraversedChunks_AC_Buffer);
+            rbSimShader.SetBuffer(2, "StickynessReqsAPPEND", m.StickynessReqs_AC_Buffer);
+
+            rbSimShader.SetBuffer(3, "RBData", m.RBDataBuffer);
+            rbSimShader.SetBuffer(3, "RBVector", m.RBVectorBuffer);
+        }
+    }
+
+    public void SetRenderShaderBuffers(ComputeShader renderShader)
+    {
+        if (m.ParticlesNum != 0) {
+            renderShader.SetBuffer(0, "SpatialLookup", m.SpatialLookupBuffer);
+            renderShader.SetBuffer(0, "StartIndices", m.StartIndicesBuffer);
+
+            renderShader.SetBuffer(0, "PData", m.PDataBuffer);
+            renderShader.SetBuffer(0, "PTypes", m.PTypesBuffer);
+        }
+        if (m.RBodiesNum != 0)
+        {
+            renderShader.SetBuffer(0, "RBData", m.RBDataBuffer);
+            renderShader.SetBuffer(0, "RBVector", m.RBVectorBuffer);
+        }
+    }
+
+    public void SetSortShaderBuffers(ComputeShader sortShader)
+    {
+        sortShader.SetBuffer(0, "SpatialLookup", m.SpatialLookupBuffer);
+
+        sortShader.SetBuffer(0, "PData", m.PDataBuffer);
+        sortShader.SetBuffer(0, "PTypes", m.PTypesBuffer);
+
+        sortShader.SetBuffer(1, "SpatialLookup", m.SpatialLookupBuffer);
+
+        sortShader.SetBuffer(1, "PData", m.PDataBuffer);
+        sortShader.SetBuffer(1, "PTypes", m.PTypesBuffer);
+
+        sortShader.SetBuffer(2, "StartIndices", m.StartIndicesBuffer);
+
+        sortShader.SetBuffer(3, "SpatialLookup", m.SpatialLookupBuffer);
+        sortShader.SetBuffer(3, "StartIndices", m.StartIndicesBuffer);
+        sortShader.SetBuffer(3, "PTypes", m.PTypesBuffer);
+        sortShader.SetBuffer(3, "PData", m.PDataBuffer);
+
+        sortShader.SetBuffer(4, "StickynessReqsCONSUME", m.StickynessReqs_AC_Buffer);
+        sortShader.SetBuffer(4, "SortedStickyRequests", m.SortedStickyRequestsBuffer);
+
+        sortShader.SetBuffer(5, "SortedStickyRequests", m.SortedStickyRequestsBuffer);
+    }
+
+    public void SetMarchingSquaresShaderBuffers(ComputeShader marchingSquaresShader)
+    {
+        marchingSquaresShader.SetBuffer(0, "MSPoints", m.MSPointsBuffer);
+        marchingSquaresShader.SetBuffer(0, "SpatialLookup", m.SpatialLookupBuffer);
+        marchingSquaresShader.SetBuffer(0, "StartIndices", m.StartIndicesBuffer);
+
+        marchingSquaresShader.SetBuffer(0, "PData", m.PDataBuffer);
+        marchingSquaresShader.SetBuffer(0, "PTypes", m.PTypesBuffer);
+        
+        marchingSquaresShader.SetBuffer(1, "Vertices", m.VerticesBuffer);
+        marchingSquaresShader.SetBuffer(1, "Triangles", m.TrianglesBuffer);
+        marchingSquaresShader.SetBuffer(1, "Colors", m.ColorsBuffer);
+        marchingSquaresShader.SetBuffer(1, "MSPoints", m.MSPointsBuffer);
+    }
+
+    public void UpdatePSimShaderVariables(ComputeShader pSimShader)
+    {
+        pSimShader.SetInt("MaxInfluenceRadiusSqr", m.MaxInfluenceRadiusSqr);
+        pSimShader.SetFloat("InvMaxInfluenceRadius", m.InvMaxInfluenceRadius);
+        pSimShader.SetInt("ChunkNumW", m.ChunkNumW);
+        pSimShader.SetInt("ChunkNumH", m.ChunkNumH);
+        pSimShader.SetInt("IOOR", m.IOOR);
+        pSimShader.SetInt("SIOOR", m.SIOOR);
+        pSimShader.SetInt("Width", m.Width);
+        pSimShader.SetInt("Height", m.Height);
+        pSimShader.SetInt("ParticlesNum", m.ParticlesNum);
+        pSimShader.SetInt("MaxInfluenceRadius", m.MaxInfluenceRadius);
+        pSimShader.SetInt("SpawnDims", m.SpawnDims);
+        pSimShader.SetInt("TimeStepsPerRender", m.TimeStepsPerRender);
+        pSimShader.SetFloat("LookAheadFactor", m.LookAheadFactor);
+        pSimShader.SetFloat("BorderPadding", m.BorderPadding);
+        pSimShader.SetFloat("MaxInteractionRadius", m.MaxInteractionRadius);
+        pSimShader.SetFloat("InteractionAttractionPower", m.InteractionAttractionPower);
+        pSimShader.SetFloat("InteractionFountainPower", m.InteractionFountainPower);
+        pSimShader.SetInt("SpringCapacity", m.SpringCapacity);
+        pSimShader.SetFloat("Plasticity", m.Plasticity);
+        
+        // Set math resources constants
+    }
+
+    public void UpdateRbSimShaderVariables(ComputeShader rbSimShader)
+    {
+        rbSimShader.SetInt("ChunkNumW", m.ChunkNumW);
+        rbSimShader.SetInt("ChunkNumH", m.ChunkNumH);
+        rbSimShader.SetInt("Width", m.Width);
+        rbSimShader.SetInt("Height", m.Height);
+        rbSimShader.SetInt("ParticlesNum", m.ParticlesNum);
+        rbSimShader.SetInt("RBodiesNum", m.RBodiesNum);
+        rbSimShader.SetInt("MaxInfluenceRadius", m.MaxInfluenceRadius);
+        rbSimShader.SetInt("MaxChunkSearchSafety", m.MaxChunkSearchSafety);
+
+        rbSimShader.SetFloat("Damping", m.Damping);
+        rbSimShader.SetFloat("Gravity", m.Gravity);
+        rbSimShader.SetFloat("RbElasticity", m.RbElasticity);
+        rbSimShader.SetFloat("BorderPadding", m.BorderPadding);
+    }
+
+    public void UpdateRenderShaderVariables(ComputeShader renderShader)
+    {
+        renderShader.SetFloat("VisualParticleRadii", m.VisualParticleRadii);
+        renderShader.SetFloat("RBRenderThickness", m.RBRenderThickness);
+        renderShader.SetInt("ResolutionX", m.ResolutionX);
+        renderShader.SetInt("ResolutionY", m.ResolutionY);
+        renderShader.SetInt("Width", m.Width);
+        renderShader.SetInt("Height", m.Height);
+        renderShader.SetInt("MaxInfluenceRadius", m.MaxInfluenceRadius);
+        renderShader.SetInt("ChunkNumW", m.ChunkNumW);
+        renderShader.SetInt("ChunkNumH", m.ChunkNumH);
+        renderShader.SetInt("ParticlesNum", m.ParticlesNum);
+        renderShader.SetInt("RBodiesNum", m.RBodiesNum);
+        renderShader.SetInt("RBVectorNum", m.RBVectorNum);
+        
+    }
+
+    public void UpdateSortShaderVariables(ComputeShader sortShader)
+    {
+        sortShader.SetInt("MaxInfluenceRadius", m.MaxInfluenceRadius);
+        sortShader.SetInt("ChunkNumW", m.ChunkNumW);
+        sortShader.SetInt("IOOR", m.IOOR);
+    }
+
+    public void UpdateMarchingSquaresShaderVariables(ComputeShader marchingSquaresShader)
+    {   
+        marchingSquaresShader.SetInt("MarchW", m.MarchW);
+        marchingSquaresShader.SetInt("MarchH", m.MarchH);
+        marchingSquaresShader.SetFloat("MSResolution", m.MSResolution);
+        marchingSquaresShader.SetInt("MaxInfluenceRadius", m.MaxInfluenceRadius);
+        marchingSquaresShader.SetInt("ChunkNumW", m.ChunkNumW);
+        marchingSquaresShader.SetInt("ChunkNumH", m.ChunkNumH);
+        marchingSquaresShader.SetInt("Width", m.Width);
+        marchingSquaresShader.SetInt("Height", m.Height);
+        marchingSquaresShader.SetInt("ParticlesNum", m.ParticlesNum);
+        marchingSquaresShader.SetFloat("MSvalMin", m.MSvalMin);
+        marchingSquaresShader.SetFloat("TriStorageLength", m.TriStorageLength);
+    }
+}
