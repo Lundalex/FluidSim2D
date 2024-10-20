ed682e2c6a95bd95ad1b238826779b3f25c0ef76
abbnoamar <Noah.Marklund@abbgymnasiet.se>
2024-10-20 21:50:09 +0200

    A couple RB sensors

---
+++ Simulation/Assets/Scripts/C#/Scene Objects/Sensors/Abstract/RigidBodySensor.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public abstract class RigidBodySensor : Sensor
+{
+    public abstract void UpdateSensorContents(RBData[] rBDatas, int linkedRBIndex);
+
+    public override void UpdateSensor()
+    {
+        if (sensorText != null)
+        {
+            if (linkedRBIndex == -1) Debug.LogWarning("Sensor not linked to any rigid body. Sensor UI will not be updated");
+            else
+            {
+                RBData[] retrievedRBDatas = sensorManager.retrievedRBDatas;
+                RBData rbData = retrievedRBDatas[linkedRBIndex];
+                targetPosition = rbData.pos + offset;
+
+                // Init sensor UI position
+                if (!firstDataRecieved) sensorUIRect.localPosition = SimSpaceToCanvasSpace(targetPosition);
+                firstDataRecieved = true;
+
+                UpdateSensorContents(retrievedRBDatas, linkedRBIndex);
+            }
+        }
+    }
+}
