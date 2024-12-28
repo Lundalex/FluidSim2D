4e6a2aa6e2ac6dc8691d4011e54c3a358e77ee8d
Noah <Noah.Marklund0@gmail.com>
2024-12-29 01:30:56 +0700

    Added linked resources

--- Simulation/Assets/Scripts/C#/RuntimeUI/UserInputUI/URLTriggerButtonManager.cs
+++ Simulation/Assets/Scripts/C#/RuntimeUI/UserInputUI/URLTriggerButtonManager.cs
@@ -3,7 +3,10 @@ using UnityEngine.UI;
 
 public class UserURL : MonoBehaviour
 {
+    [Header("URL")]
     [SerializeField] private string url = "URL Here";
+
+    [Header("Refrences")]
     [SerializeField] private Image backgroundImage;
 
     void Awake()
